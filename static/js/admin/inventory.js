fbd0ebd3a9efe5767d13c37338362a7624a39454
glamorousangel <dmilesang@gmail.com>
2026-03-14 23:35:32 +0800

    inventory done

--- static/js/admin/inventory.js
+++ static/js/admin/inventory.js
@@ -7,6 +7,9 @@ const addProductBtn = document.getElementById("addProductBtn");
 const addProductModal = document.getElementById("addProductModal");
 const closeModal = document.getElementById("closeModal");
 
+const imageInput = document.getElementById("productImage");
+const preview = document.getElementById("imagePreview");
+
 // Sidebar toggle
 menuBtn.onclick = () => {
 sidebar.classList.add("open");
@@ -39,3 +42,17 @@ addProductModal.classList.remove("show");
 sidebar.classList.remove("open");
 overlay.classList.remove("show");
 }
+
+imageInput.addEventListener("change", function(){
+    const file = this.files[0];
+
+    if(file){
+        const reader = new FileReader();
+
+        reader.onload = function(e){
+            preview.innerHTML = `<img src="${e.target.result}">`;
+        }
+
+        reader.readAsDataURL(file);
+    }
+});
